44146c30e187d6a0d6638d71823d6df339cfdf77
Santa Zukovska <santa.zukovska@mintos.com>
2021-04-01 11:13:29 +0300

    Started to build App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="outerContainer">
+      <div class="gridContainerSelected"></div>
+
+      <div
+        class="gridContainer"
+      >
+        <button class="button" v-for="currency in currencies"
+        :key="currency.name">{{currency.name}}</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./app.css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      currencies: [
+        { id: 1, name: "EUR", isSelected: false },
+        { id: 2, name: "PLN", isSelected: false },
+        { id: 3, name: "GEL", isSelected: false },
+        { id: 4, name: "DKK", isSelected: false },
+        { id: 5, name: "CZK", isSelected: false },
+        { id: 6, name: "GBP", isSelected: false },
+        { id: 7, name: "SEK", isSelected: false },
+        { id: 8, name: "USD", isSelected: false },
+        { id: 9, name: "RUB", isSelected: false },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/Currency.vue
@@ -0,0 +1,14 @@
+<template>
+  
+    
+</template>
+
+<script>
+export default {
+  name: 'Currency',
+  props: {
+    
+  }
+}
+</script>
+
