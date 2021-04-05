98125b1e9c6cec7e14d73236e4bea4018969eacf
Santa Zukovska <zukovska.santa@gmail.com>
2021-04-05 20:03:49 +0300

    Added sort() method

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
   <div id="app">
     <div class="outerContainer">
-      <div class="gridContainerSelected"></div>
-
-      <div
-        class="gridContainer"
-      >
-        <button class="button" v-for="currency in currencies"
-        :key="currency.name">{{currency.name}}</button>
+      <div class="gridContainerSelected">
+        <div
+          class="selectedCurrency"
+          v-for="selectedCurrency in selectedCurrencies"
+          :key="selectedCurrency.id"
+        >
+          <button
+            aria-label="Close"
+            class="closeButton"
+            v-on:click="select(selectedCurrency.id)"
+          >
+            <span aria-hidden="true">×</span>
+          </button>
+          {{ selectedCurrency.name }}
+        </div>
+      </div>
+      <div class="gridContainer">
+        <button
+          class="button"
+          v-bind:class="{ selected: currency.isSelected }"
+          v-for="currency in currencies"
+          :key="currency.id"
+          v-on:click="select(currency.id)"
+        >
+          <div class="fakeCheckbox"></div>
+          <cross />
+          {{ currency.name }}
+        </button>
       </div>
     </div>
   </div>
@@ -15,26 +36,45 @@
 
 <script>
 import "./app.css";
+import cross from "./components/cross.vue";
 
 export default {
   name: "App",
-  components: {},
+  components: { cross },
   data() {
     return {
       currencies: [
-        { id: 1, name: "EUR", isSelected: false },
-        { id: 2, name: "PLN", isSelected: false },
-        { id: 3, name: "GEL", isSelected: false },
-        { id: 4, name: "DKK", isSelected: false },
-        { id: 5, name: "CZK", isSelected: false },
-        { id: 6, name: "GBP", isSelected: false },
-        { id: 7, name: "SEK", isSelected: false },
-        { id: 8, name: "USD", isSelected: false },
-        { id: 9, name: "RUB", isSelected: false },
+        { id: 1, name: "EUR", isSelected: false, selectedAt: null },
+        { id: 2, name: "PLN", isSelected: false, selectedAt: null },
+        { id: 3, name: "GEL", isSelected: false, selectedAt: null },
+        { id: 4, name: "DKK", isSelected: false, selectedAt: null },
+        { id: 5, name: "CZK", isSelected: false, selectedAt: null },
+        { id: 6, name: "GBP", isSelected: false, selectedAt: null },
+        { id: 7, name: "SEK", isSelected: false, selectedAt: null },
+        { id: 8, name: "USD", isSelected: false, selectedAt: null },
+        { id: 9, name: "RUB", isSelected: false, selectedAt: null },
       ],
     };
   },
+  methods: {
+    select(id) {
+      this.currencies.map((currency) => {
+        if (currency.id === id) {
+          currency.isSelected = !currency.isSelected;
+          currency.selectedAt = new Date();
+        }
+        return currency;
+      });
+    },
+  },
+  computed: {
+    selectedCurrencies: function() {
+      const selected = this.currencies
+        .filter((item) => item.isSelected === true)
+        .sort((a, b) => a.selectedAt.getTime() - b.selectedAt.getTime());
+
+      return selected;
+    },
+  },
 };
 </script>
-
-
---
+++ src/components/cross.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="9.819"
+          height="13.399"
+          viewBox="0 0 9.819 13.399"
+        >
+          <g
+            id="Component_1_1"
+            data-name="Component 1 – 1"
+            transform="translate(0.409 0.287)"
+          >
+            <line
+              id="Line_2"
+              data-name="Line 2"
+              x2="9"
+              y2="12.825"
+              fill="none"
+              stroke="red"
+              strokeWidth="1"
+            />
+            <line
+              id="Line_3"
+              data-name="Line 3"
+              x1="9"
+              y2="12.825"
+              fill="none"
+              stroke="red"
+              strokeWidth="1"
+            />
+          </g>
+        </svg>
+</template>
